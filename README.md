5649cb121d4a887a0b62dffc949f0c0cb1fc85c8
Tiur Mawaddah <tiurzm@gmail.com>
2019-10-05 01:04:24 -0500

    <COMMENT>

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Trivia-Game-2
+
+ if(answerValue === correctAnswer){
+                    var correctImage = $("<img>");
+                    correctImage.addClass("correct")
+                    correctImage.attr("src", "assets/images/dm.gif");
+                    $(".correct").append(newImage);
+                }else {
+                    var wrongImage = $("<img>");
+                    wrongImage.addClass("wrong");
+                    wrongImage.attr("src", "assets/images/md.gif")
+                    $(".wrong").append(wrongImage);
+                }
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-    // Disney movie, source https://ohmy.disney.com/quiz/2016/09/02/quiz-guess-the-disney-movie-pop-culture-references/
+    // Disney movie, source https://www.buzzfeed.com/briangalindo/the-ultimate-disney-trivia-quiz
     var allQuestion =[
         {
-            question: "In 'The Little Mermaid', who is NOT one of Triton’s daughter?",
+            question: "In “The Little Mermaid”, who is NOT one of Triton’s daughter?",
             answers: ["Andrina", "Adora", "Attina", "Alana"],
             correctAnswer: "Adora",
         }, {
-            question: "Which phrase does the Evil Queen in 'Snow White' actually say??",
-            answers: ["“Mirror, mirror, on the wall — who is the fairest of them all?”", "“Magic mirror, on the wall — who is the fairest one of all?”", "“Magic mirror, on the wall — who is the fairest one of all?”", "“Magic mirror, on the wall — who is the fairest of them all?”"],
-            correctAnswer: "“Magic mirror, on the wall — who is the fairest one of all?”",
+            question: "On whose shoulders does Dopey stand on in order to dance with Snow White during “The Silly Song” scene?",
+            answers: ["Doc", "Grumpy", "Sneezy", "Happy"],
+            correctAnswer: "Sneezy",
         }, {
             question: "In the movie “Tangled”, Flynn Rider is wanted dead or alive according to his wanted poster because he's a?",
             answers: ["Bandit", "Thief", "Treasonist", "Robber"],
@@ -18,69 +18,106 @@ $(document).ready(function(){
             answers: ["Diablo", "Malum", "Mauvais", "Diable"],
             correctAnswer: "Mauvais",
         }, {
-            question: "In “Pocahontas”, what did Pocahontas see in her dream that made her believe that a change was coming?",
-            answers: ["A spinning arrow", "A strange cloud formation", "A hawk circling her village", "A burning blue fire"],
-            correctAnswer: "A spinning arrow",
+            question: "In “Aladdin“, what does Aladdin, and a reluctant Abu, give to the poor children to eat?",
+            answers: ["Dates", "Apples", "Bread", "Cheese"],
+            correctAnswer: "Bread",
         }, 
     ];
     
-    console.log(allQuestion[0].answers);
+    // console.log(allQuestion[0].answers);
 
     
     var currentQuestion = 0;
+    var timeRemain = 15;
     var intervalId;
-    var right = "";
-    var wrong = "";
-    var none = ""; 
 
     $(".start").on("click", function(){
         $(this).remove();
         var newDiv = $("<div>");
         newDiv.addClass("timer");
         $("#target").append(newDiv);
-        $(".timer").append("<p>Time remaining:<span class=time> 30</span> seconds</p>");
+        $(".timer").append("<p>Time remaining:<span class=time> 15</span> seconds</p>");
         var question1 = $("<form>");
         question1.addClass("question");
         $(".timer").append(question1);
         loadQueswer();
-        // run the time
-        
-        
-        
-        // display question and answer:
-        function loadQueswer(){
-            var timeRemain = 30;
-            var choices = allQuestion[currentQuestion].answers;
-            $(".question").append("<p>"+ allQuestion[currentQuestion].question +"</p>");
-            for(var i=0; i < choices.length; i++){
-                $(".question").append("<p class=choice>" + choices[i] +"</p>");
-            }
-            run();
-            $(".choice").on("click", function(){
-                stop();
-            });
-            // set timer
-            function run(){
-                clearInterval(intervalId);
-                intervalId = setInterval(decrement, 1000);
-            };
-            // decrement the time
-            function decrement(){
-                timeRemain--;
-                $(".time").html(timeRemain);
-                if(timeRemain === 0){
-                    stop();
-                    alert("Time Up!");
-                }
-            };
-            // stop time
-            function stop(){
-                clearInterval(intervalId);
-                timeRemain = 30;
-                $(".time").html(timeRemain);
-            }
-
-        };
-        
     });
+
+    var answerValue = $(this).attr("value");
+    console.log(answerValue);
+    var correctAnswer = allQuestion[currentQuestion].correctAnswer;
+    console.log(correctAnswer);
+
+    // display question and answer:
+        function loadQueswer(){
+        var question = allQuestion[currentQuestion].question;
+        var putQuestion = $("<p>");
+        putQuestion.text(question);
+        $(".question").append(putQuestion);
+        var choices = allQuestion[currentQuestion].answers;
+        for(var i=0; i < choices.length; i++){
+            var displayQuestion = $("<p>");
+            displayQuestion.addClass("choice");
+            displayQuestion.attr("value", choices[i]);
+            displayQuestion.text(choices[i]);
+            $(".question").append(displayQuestion);
+        }
+        run();
+        decrement();
+        $(".choice").on("click", function(){
+            stop();
+            var done = $("<div>");
+            done.addClass("display");
+            $(".timer").append(done);
+           
+            if(answerValue === correctAnswer){
+                var correctP = $("<p>");
+                correctP.text("Correct");
+                var correctImage = $("<img>");
+                correctImage.addClass("correct")
+                correctImage.attr("src", "assets/images/md.gif");
+                $(".display").append(correctP, correctImage);
+            }else{
+                var wrongP = $("<p>");
+                wrongP.text("Nope!");
+                var wrongC = $("<p>");
+                wrongC.text("The Correct Answer was: " + correctAnswer);
+                var wrongImage = $("<img>");
+                wrongImage.addClass("wrong");
+                wrongImage.attr("src", "assets/images/dm.gif")
+                $(".display").append(wrongP, wrongC, wrongImage);
+            }
+            $(".question").remove();
+        });
+    };
+
+    // set timer
+    function run(){
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+    };
+    // decrement the time
+    function decrement(){
+        timeRemain--;
+        $(".time").html(timeRemain);
+        if(timeRemain === 0){
+            stop();
+            $(".question").remove();
+            alert("Time Up!");
+            var timeUp = $("<p>");
+            timeUp.text("Nope!");
+            var timeOut = $("<p>");
+            timeOut.text("The Correct Answer was: " + correctAnswer);
+            var timeImage = $("<img>");
+            timeImage.addClass("time");
+            timeImage.attr("src", "assets/images/dm.gif")
+            $(".timer").append(timeUp, timeOut, timeImage);
+        }
+    };
+    // stop time
+    function stop(){
+        clearInterval(intervalId);
+        timeRemain = timeRemain;
+        $(".time").html(timeRemain);
+    }
 });
